6f7afe9a86cdb428c4e33978bd014d8c49db5216
Neto Concon <concon.neto2@gmail.com>
2018-11-16 11:16:42 -0200

    add username to dropdown

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -14,6 +14,6 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def user_params
-    params.require(:user).permit(:email, :photo)
+    params.require(:user).permit(:email, :photo, :username)
   end
 end
