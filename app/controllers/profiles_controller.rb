d041bf80f4445c978c466eb1f67f80faae6d5972
Felipe Santos <santos.felipe13@gmail.com>
2018-11-20 19:31:09 -0200

    added coloring and ranking to users

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -33,7 +33,7 @@ class ProfilesController < ApplicationController
       end
 
       max_relevance = @relevances.max
-      @relevances.map! { |val| ((val.to_f / max_relevance) * 100).round(1)  }
+      @relevances.map! { |val| ((val.to_f / max_relevance) * 100).round(1) }
 
     end
   end
